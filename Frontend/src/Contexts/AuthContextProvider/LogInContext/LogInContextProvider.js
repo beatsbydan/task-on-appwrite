15a40ea7c059e28b7c58f94549f25c33f23b795a
Onyeachonam Daniel <danielonyeachonam2020@gmail.com>
2023-06-16 02:26:46 +0100

    Done

--- Frontend/src/Contexts/AuthContextProvider/LogInContext/LogInContextProvider.js
+++ Frontend/src/Contexts/AuthContextProvider/LogInContext/LogInContextProvider.js
@@ -11,7 +11,7 @@ const LogInContextProvider = (props) => {
     const handleVisibility = () => {
         setVisible(!visible)
     }
-    const user_api = 'https://task-on-production.up.railway.app/api/users/'
+    // const user_api = 'https://task-on-production.up.railway.app/api/users/'
     const logIn_api = 'https://task-on-production.up.railway.app/api/auth/login'
     const [logInErrors, setLogInErrors] = useState({})
     const handleSubmit = async () => {
@@ -31,37 +31,40 @@ const LogInContextProvider = (props) => {
                         email: '',
                         password: ''
                     })
+                    const myData = res.data.data
+                    console.log(myData)
+                    const userData = {
+                        email:myData.email,
+                        firstname: myData.firstname,
+                        lastname:myData.lastname,
+                        profileImage: myData.profile_image,
+                        myToken: myData.token,
+                        myId: myData.user.$id
+                    }
+                    console.log(userData)
+                    localStorage.setItem('user', JSON.stringify(userData))
                     localStorage.setItem('myToken', res.data.data.token)
                     localStorage.setItem('myId', res.data.data.user.$id)
                     success.logInSuccess = true
-                    axios.get(user_api + res.data.data.user.$id, {
-                        headers: {
-                            'Authorization': `Bearer ${res.data.data.token}`
-                        }
-                    })
-                    .then(res=>{
-                        console.log(res)
-                        if(res.status === 200){
-                            const myData = res.data.data.user
-                            console.log(myData)
-                            const userData = {
-                                email:myData.email,
-                                firstname: myData.firstname,
-                                lastname:myData.lastname,
-                                profileImage: myData.profile_image
-                            }
-                            console.log(userData)
-                            localStorage.setItem('user', JSON.stringify(userData))
-                        }
-                        else{
-                            alert('SOMETHING WENT WRONG')
-                            localStorage.clear()
-                            success.logInSuccess = false
-                        }
-                    })
-                    .catch(error=>{
-                        console.log(error)
-                    })
+                    // axios.get(user_api + res.data.data.user.$id, {
+                    //     headers: {
+                    //         'Authorization': `Bearer ${res.data.data.token}`
+                    //     }
+                    // })
+                    // .then(res=>{
+                    //     console.log(res)
+                    //     if(res.status === 200){
+                            
+                    //     }
+                    //     else{
+                    //         alert('SOMETHING WENT WRONG')
+                    //         localStorage.clear()
+                    //         success.logInSuccess = false
+                    //     }
+                    // })
+                    // .catch(error=>{
+                    //     console.log(error)
+                    // })
                 }
                 if(res.status === 401){
                     success.logInSuccess = false
